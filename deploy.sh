8df4abf5748206f9aff6b009db890de979eee1f3
jarno <44927472+rambojunior005@users.noreply.github.com>
2025-11-23 22:49:48 +0100

    dashboard update

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker-compose down
-docker-compose build
-docker-compose up -d
-echo "Deployed! Check http://localhost:9000 (Portainer)"
--- update.sh
+++ update.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
-docker-compose build
 docker-compose down
+docker-compose build
 docker-compose up -d
 echo "Updated!"
-```
-
-Maak executable: `chmod +x deploy.sh update.sh`
-
-## 2. `.env` file voor secrets
-
-`.env`:
-```
-INFLUXDB_PASSWORD=yourpassword
-INFLUXDB_TOKEN=yoursupersecrettoken
